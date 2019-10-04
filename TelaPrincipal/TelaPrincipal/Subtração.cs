802b79289f74ccc702f6bcbb4bd54083a23818c5
Eduardo <informatica.cnec1a@gmail.com>
2019-10-03 22:08:07 -0300

    lala

---
+++ TelaPrincipal/TelaPrincipal/Subtração.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TelaPrincipal
+{
+    public partial class Subtração : Form
+    {
+        public Subtração()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int valor1 = Convert.ToInt32(textBox1.Text);
+            int valor2 = Convert.ToInt32(textBox2.Text);
+
+            int resultado = valor1 - valor2;
+
+            textBox3.Text = resultado.ToString();
+        }
+    }
+}
